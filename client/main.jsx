e17a3230ce4e21e9398ea7c1e8ce73572d0a91d5
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-20 17:39:27 -0800

    Render into wrapper div.

--- client/main.jsx
+++ client/main.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/App';
 
-ReactDOM.render(<App/>, document.getElementsByTagName('body')[0]);
+ReactDOM.render(<App/>, document.getElementById('wrapper'));
