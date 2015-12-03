ebde3b8d268578e84bc3d1f1df5641cf26c48f5c
Dan Lipsitt <danlipsitt@gmail.com>
2015-12-02 18:44:32 -0800

    Shallow render.

--- client/test/app.jsx
+++ client/test/app.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
 import chai from 'chai';
 should = chai.should();
 import App from '../components/App';
 
+let shallowRenderer = TestUtils.createRenderer();
+
 describe('App', function() {
   it('should render', function() {
-    let x = 1;
-    x.should.equal(1);
+    shallowRenderer.render(<App/>);
+    let result = shallowRenderer.getRenderOutput();
+    result.should.be.ok;
   });
 });
